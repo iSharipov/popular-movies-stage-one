916f85720f1736fd66e7e91866e6c1a79fccd69c
ilias <iliamsharipov@gmail.com>
2018-02-18 14:19:44 +0300

    #master added DetailActivity xml description added DetailActivity class application tag lint refactoring

---
+++ app/src/main/java/popularmoviesstageone/udacity/com/popular_movies_stage_one/DetailActivity.java
@@ -0,0 +1,10 @@
+package popularmoviesstageone.udacity.com.popular_movies_stage_one;
+
+import android.app.Activity;
+
+/**
+ * Created by ilias on 18.02.2018.
+ */
+
+public class DetailActivity extends Activity {
+}
